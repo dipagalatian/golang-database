fbf91f7f960b2d02fcf590c922563483247c0bd5
dipagalatian <dipagalatian3003@gmail.com>
2026-01-12 23:17:54 +0700

    FEAT: Implement parameters for query to avoid Sql Injection Issue

--- sql_test.go
+++ sql_test.go
@@ -160,3 +160,58 @@ func TestSqlInjection(t *testing.T) {
 	}
 	
 }
+
+func TestSqlInjectionSafe(t *testing.T) {
+
+	db := GetConnectionDB()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #"
+	password := "fasdf"
+
+	script := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1;"
+	fmt.Println("script login:", script)
+	
+	rows, err  := db.QueryContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		
+		fmt.Println("Login success:", username)
+	} else {
+		fmt.Println("Login Failed")
+	}
+	
+}
+
+func TestExecSqlParameter(t *testing.T) {
+
+	db := GetConnectionDB()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "dipa"
+	password := "dipa"
+
+	script := "INSERT INTO user(username, password) VALUES (?, ?);"
+
+	_, err  := db.ExecContext(ctx, script, username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert data user")
+	
+}
